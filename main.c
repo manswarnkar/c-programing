e1a21dff33c3ffdd8b787880128ddb01afac02d9
manmohan soni <manmohansoni1094@gmail.com>
2024-08-06 13:43:08 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter the value of a and b ");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("%d",c);
+    
+    return 0;
+}
